c5e632cd5c018e9d4bde10e404c513c0c9f2958a
Filip <f@filip.world>
2024-03-04 14:25:38 -0500

    various improvements and spam checking

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os/signal"
 	"slices"
 	"strings"
+	"sync"
 	"syscall"
 	"time"
 	"unicode/utf8"
@@ -16,11 +17,11 @@ import (
 )
 
 const (
-	JUICEBOX_GUILD_ID     = "775859454780244028"
-	CONTRIBUTOR_ROLE_ID   = "865459358434590740"
-	ADMIN_ROLE_ID         = "864238986456989697"
-	ALUMNI_ROLE_ID        = "1091786430046552097"
-	OPERATIONS_CHANNEL_ID = "889560116666441748"
+	juiceboxGuildId     = "775859454780244028"
+	contributorRoleId   = "865459358434590740"
+	adminRoleId         = "864238986456989697"
+	alumniRoleId        = "1091786430046552097"
+	operationsChannelId = "889560116666441748"
 )
 
 var s *discordgo.Session
@@ -56,7 +57,9 @@ func main() {
 
 	buildContributorsList()
 	s.AddHandler(userJoins)
-	log.Println("Now monitoring new users.")
+	s.AddHandler(nickChange)
+	s.AddHandler(checkSpam)
+	log.Println("Now monitoring the server.")
 
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, os.Interrupt, syscall.SIGTERM)
@@ -79,7 +82,7 @@ func buildContributorsList() {
 	// Add nicknames and usernames for users with contributor, admin, or alumni roles to the map
 	var after string
 	for {
-		mems, err := s.GuildMembers(JUICEBOX_GUILD_ID, after, 1000)
+		mems, err := s.GuildMembers(juiceboxGuildId, after, 1000)
 		if err != nil {
 			log.Fatalf("Error getting guild members: %s\n", err)
 		}
@@ -87,7 +90,7 @@ func buildContributorsList() {
 	memLoop:
 		for _, mem := range mems {
 			for _, r := range mem.Roles {
-				if r == CONTRIBUTOR_ROLE_ID || r == ADMIN_ROLE_ID || r == ALUMNI_ROLE_ID {
+				if r == contributorRoleId || r == adminRoleId || r == alumniRoleId {
 					username := strings.ToLower(mem.User.Username)
 					nick := strings.ToLower(mem.Nick)
 
@@ -119,37 +122,132 @@ func buildContributorsList() {
 	log.Printf("Built contributors list with %d entries: %v\n", len(contributors), contributors)
 }
 
-// A handler to check whether a user joining is suspicious. If they are, mute them for 24 hours and send a message saying they were muted.
+// Holds recent message information for a user
+type RecentSpam struct {
+	count      int
+	channelIds []string
+	msgs       []string
+}
+
+// Map of user IDs to the number of messages they've sent in the last 100 seconds
+var spamTracker = struct {
+	sync.RWMutex // Fine to have one lock for the whole struct since reads/writes are infrequent. At scale, would need to optimize.
+	recent       map[string]RecentSpam
+}{
+	recent: make(map[string]RecentSpam),
+}
+
+// When a recently joined user sends a message, check if they've sent messages to many channels recently, and mute them if they have.
+func checkSpam(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// If the user joined more than a week ago, don't check their messages
+	if time.Since(m.Member.JoinedAt) > time.Hour*24*7 {
+		return
+	}
+
+	// If the author is a bot or nil, return
+	if m.Author.Bot || m.Author == nil {
+		return
+	}
+
+	// If the user has a contributor, admin, or alumni role, don't check their messages
+	for _, r := range m.Member.Roles {
+		if r == contributorRoleId || r == adminRoleId || r == alumniRoleId {
+			return
+		}
+	}
+
+	spamTracker.RLock()
+	r, ok := spamTracker.recent[m.Author.ID]
+	spamTracker.RUnlock()
+
+	// If not found, initialize the user's spam tracker
+	if !ok {
+		spamTracker.Lock()
+		spamTracker.recent[m.Author.ID] = RecentSpam{
+			count:      1,
+			channelIds: []string{m.ChannelID},
+			msgs:       []string{m.Content},
+		}
+		spamTracker.Unlock()
+
+		// After 2 minutes, clear the spam tracker for this user
+		go func() {
+			time.Sleep(2 * time.Minute)
+			spamTracker.Lock()
+			delete(spamTracker.recent, m.Author.ID)
+			spamTracker.Unlock()
+		}()
+	} else {
+		// If the user has sent more than 3 messages in the past 2 minutes, investigate further
+		if len(r.msgs) > 3 {
+			slices.Sort(r.msgs)
+			compactMsgs := slices.Compact(r.msgs)
+			// If the compact slice is shorter than the original, the user has sent the same message multiple times
+			if len(compactMsgs) < len(r.msgs) {
+				// So we mute them
+				muteTime := time.Now().Add(1 * time.Hour)
+				muteMsg := fmt.Sprintf("%s sent %d messages in the last 2 minutes. Muting until <t:%d>. In channels: ", m.Author.Mention(), r.count, muteTime.Unix())
+				for _, c := range slices.Compact(r.channelIds) {
+					muteMsg += fmt.Sprintf("<#%s> ", c)
+				}
+				muteMsg += ". Most recent content: " + r.msgs[len(r.msgs)-1]
+				muteMember(m.Author.ID, muteMsg, muteTime)
+			}
+		}
+
+		spamTracker.Lock()
+		spamTracker.recent[m.Author.ID] = RecentSpam{
+			count:      r.count + 1,
+			channelIds: append(r.channelIds, m.ChannelID),
+			msgs:       append(r.msgs, m.Content),
+		}
+		spamTracker.Unlock()
+	}
+}
+
+// When a user joins, check if their nickname is suspicious, and mute them if it is.
 func userJoins(s *discordgo.Session, m *discordgo.GuildMemberAdd) {
-	muteTime := time.Now().Add(24 * time.Hour)
-	var sus bool
-	var msg string
-
 	if is, match := isSus(m.User.Username); is {
-		sus = true
-		msg = fmt.Sprintf("User <@%s> joined with a suspicious username (close to %s). Muting until <t:%d>.", m.User.Username, match, muteTime.Unix())
-		log.Println(msg)
-		goto Mute
+		muteTime := time.Now().Add(24 * time.Hour)
+		muteMsg := fmt.Sprintf("%s joined with a suspicious username ('%s', close to '%s'). Muting until <t:%d>.", m.User.Mention(), m.User.Username, match, muteTime.Unix())
+		muteMember(m.User.ID, muteMsg, muteTime)
 	}
+}
 
-	if m.Nick != "" {
-		if is, match := isSus(m.Nick); is {
-			sus = true
-			msg = fmt.Sprintf("User <@%s> joined with a suspicious nickname %s (close to %s). Muting until <t:%d>.", m.User.ID, m.Nick, match, muteTime.Unix())
-			log.Println(msg)
+// When a user changes their nickname, check if it's suspicious and mute them if it is.
+func nickChange(s *discordgo.Session, m *discordgo.GuildMemberUpdate) {
+	// If the user has a contributor, admin, or alumni role, don't check their nickname
+	for _, r := range m.Roles {
+		if r == contributorRoleId || r == adminRoleId || r == alumniRoleId {
+			return
 		}
 	}
 
-Mute:
-	if sus {
-		if _, err := s.ChannelMessageSend(OPERATIONS_CHANNEL_ID, msg); err != nil {
-			log.Printf("Error sending message to operations channel: %s\n", err)
-		}
-
-		if err := s.GuildMemberTimeout(JUICEBOX_GUILD_ID, m.User.ID, &muteTime); err != nil {
-			log.Printf("Error muting user: %s\n", err)
-		}
+	// If the user has no nickname or didn't change their nickname, return.
+	if m.Nick == "" || m.BeforeUpdate.Nick == m.Nick {
+		return
 	}
+
+	// If the user's nickname is suspicious, mute them for 24 hours.
+	if is, match := isSus(m.Nick); is {
+		muteTime := time.Now().Add(24 * time.Hour)
+		muteMsg := fmt.Sprintf("%s switched to a suspicious nickname ('%s', close to '%s'). Muting until <t:%d>.", m.User.Mention(), m.Nick, match, muteTime.Unix())
+		muteMember(m.User.ID, muteMsg, muteTime)
+	}
+}
+
+// Mute a member and send a message to the operations channel.
+func muteMember(userId string, muteMsg string, until time.Time) {
+	if _, err := s.ChannelMessageSend(operationsChannelId, muteMsg); err != nil {
+		log.Printf("Error sending message '%s' to operations channel: %s\n", muteMsg, err)
+	}
+
+	if err := s.GuildMemberTimeout(juiceboxGuildId, userId, &until); err != nil {
+		log.Printf("Error muting user '%s' with message '%s' until %s: %s\n", userId, muteMsg, until, err)
+		return
+	}
+
+	log.Printf("Muted user %s with message %s until %s\n", userId, muteMsg, until)
 }
 
 // Checks whether the given string is suspicious and what it matches (both suspicious words and contributor names)
